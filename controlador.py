8b5e4135bb8e31b654e19e13e8c9e79559df7b97
NunoDaniel <78868722+NunoDaniel@users.noreply.github.com>
2022-11-30 12:27:53 +0000

    Update controlador.py

--- controlador.py
+++ controlador.py
@@ -10,7 +10,9 @@ from classes import *
 from db import *
 
 
-def define_sensors():
+
+
+def define_sensors(contentorId):
 
     sensors = []
     df_sensors = pd.read_csv('sensors.csv')
@@ -31,7 +33,7 @@ def define_sensors():
 
     return sensors
 
-def define_actuators():
+def define_actuators(contentorId):
 
     actuators = []
     df_actuators = pd.read_csv('actuators.csv')
@@ -101,33 +103,114 @@ def set_atuators_individual():  #para a db
     return
 
 
-def control_atuatores(sensores, ): # todos
+    
 
+def control_atuatores(sensores, atuadores,contentorid): # todos
 
+    
 
-    for sensor_ind in sensores:
+    for atuador in atuadores:
         
-        if sensor_ind[0] == "temperatura":
-            print("temp")
-        elif sensor_ind[0] == "humidade":
-            print("hum") 
-        elif sensor_ind[0] == "gas 1":
-            print("gas1")
-        elif sensor_ind[0] == "gas 2":
-            print("gas2")
+        if(atuador.name == "Frigorifico"):
+                nivel = get_nivel_regra(atuador.id)
+                control_frigorifico(sensores,nivel,atuador)
+                
+        if(atuador.name == "Ventoinha"):
+                nivel = get_nivel_regra(atuador.id)
+                control_ventoinha(sensores,nivel,atuador)
+    for atuador in atuadores:      
+             
+        if(atuador.name == "Porta"):
+                nivel = get_nivel_regra(atuador.id)
+                control_porta(sensores,nivel,atuador,contentorid)
 
-    return;
+    return
 
+def get_temperatura_info(sensores):
+    #print("get_temp_info")
+    for sens_indiv in sensores:
+        if(sens_indiv.name == "temperatura"):
+            return sens_indiv.value, sens_indiv.max, sens_indiv.min
+        
+    return None, None, None
 
-def control_frigorifico():
+def get_CO2_info(sensores):
+    #print("get_CO2_info")
+    for sens_indiv in sensores:
+        if(sens_indiv.name == "CO2"):
+            return sens_indiv.value, sens_indiv.max, sens_indiv.min
+        
+    return None, None, None
 
-    return;
+def get_O2_info(sensores):
+    #print("get_O2_info")
+    for sens_indiv in sensores:
+        if(sens_indiv.name == "O2"):
+            return sens_indiv.value, sens_indiv.max, sens_indiv.min
+        
+    return None, None, None
 
-def control_ventoina():
-    return;
+def get_humidade_info(sensores):
+    #print("get_HUM_info")
+    for sens_indiv in sensores:
+        if(sens_indiv.name == "humidade"):
+            return sens_indiv.value, sens_indiv.max, sens_indiv.min
+        
+    return None, None, None
 
-def control_porta():
-    return;
+def muda_frigorifico(valor, atuador):
+    #manda comando pro arduino
+    print(atuador.name, "mudou para ", valor)
+    return
+
+def muda_ventoinha(valor, atuador):
+    #manda comando pro arduino
+    print(atuador.name, "mudou para ", valor)
+    return
+
+def muda_porta(valor, atuador):
+    #manda comando pro arduino
+    print(atuador.name, "mudou para ", valor)
+    return
+
+def control_frigorifico(sensores, nivel, atuador):
+    
+    temp, max_, min = get_temperatura_info(sensores)
+    if(nivel == 0):         # quem manda é a raspberry
+        if(temp > max_):
+            muda_frigorifico(1,atuador)
+        elif(temp < min):
+            muda_frigorifico(0,atuador)
+    elif(nivel != 0):
+        muda_frigorifico(max(nivel,0),atuador)
+
+    return
+
+def control_ventoinha(sensores, nivel, atuador):
+    CO2, max_1, min_1 = get_CO2_info(sensores)
+    O2, max_2, min_2 = get_O2_info(sensores)
+
+    if(nivel == 0):         # quem manda é a raspberry
+        if(CO2 > max_1 or O2 > max_2):               # falta aqui hierarquia. quem manda?? CO2, O2, o minimo ou o maximo??
+            muda_ventoinha(1,atuador)
+        elif(CO2 < min_2 or CO2 < min_2):
+            muda_ventoinha(0,atuador)
+    elif(nivel != 0):
+        muda_ventoinha(max(nivel,0),atuador)
+    return
+
+def control_porta(sensores, nivel, atuador, contentorid):
+    ventoinha = get_specific_value_atuador(str(contentorid), "Ventoinha",None)
+    hum, max_, min = get_humidade_info(sensores)
+    if(nivel == 0):         # quem manda é a raspberry
+        if(hum > max_ or ventoinha == 1):
+            muda_porta(1,atuador)
+        elif(hum < min and ventoinha == 0):
+            muda_porta(0,atuador)
+    elif(nivel != 0):
+        muda_porta(max(nivel,0),atuador)
+
+    return
 
 
 def verifica_contentor(db, raspberry_id):
@@ -151,6 +234,7 @@ def verifica_contentor(db, raspberry_id):
 
 
 
+#                           Main
 
 contentor_ids = None
 raspberry_id = 1
@@ -162,14 +246,23 @@ while contentor_ids == None:
 
 
 sensor = []
+atuadores = []
+timer = []
 
 # Only called once
 for i in range(len(contentor_ids)):
     sensor.append (define_sensors(contentor_ids[i]))
+    atuadores.append (define_actuators(contentor_ids[i]))
 
+    
 while 1:
+                # limpar terminal
+    os.system('cls||clear')
+    
     for i in range(len(contentor_ids)):
-        atualiza_sensores(sensor[i])
-        control_atuatores(sensor[i])
+       atualiza_sensores(sensor[i])
+       control_atuatores(sensor[i], atuadores[i],contentor_ids[i]) # falta temporização
+
+    break
 
 
