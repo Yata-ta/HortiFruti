d61e2d6d02fa62640784df9d1801e10fce1f9505
NunoDaniel <78868722+NunoDaniel@users.noreply.github.com>
2022-12-14 17:27:55 +0000

    Update controlador.py

--- controlador.py
+++ controlador.py
@@ -22,40 +22,120 @@ def get_sensors(sensors):  #do arduino
         print('The new value from sensor ', sensor.name,' is ', sensor.value)
     return  sensors
 
+def send_db_sensor_buffer():
+    if db_connected is True:
+        data_df = pd.read_csv('sensor_data.csv')
+
+        range_for = min(20, data_df.shape[0])
+
+        for i in range(range_for):
+
+            print(i)
+
+            id = data_df.iloc[0]['id']
+            time = data_df.iloc[0]['Time']
+            value = data_df.iloc[0]['Value']
+            set_value_sensor(id, time, value)
+
+            print(id, time, value)
+
+            data_df.drop(data_df.index[0], axis=0, inplace=True)
+
+        data_df.to_csv('sensor_data.csv', index=False)
+
+    return
+
+def send_db_actuator_buffer():
+    if db_connected is True:
+        data_df = pd.read_csv('actuator_data.csv')
+
+        range_for = min(20, data_df.shape[0])
+
+        for i in range(range_for):
+
+            print(i)
+
+            id = data_df.iloc[0]['id']
+            time = data_df.iloc[0]['Time']
+            value = data_df.iloc[0]['Value']
+            set_value_atuador(id, time, value)
+
+            print(id, time, value)
+
+            data_df.drop(data_df.index[0], axis=0, inplace=True)
+
+        data_df.to_csv('actuator_data.csv', index=False)
+
+    return
+
 def define_sensors(contentorId):
-
     sensors = []
+    data_df = []
 
-    # Open db connection
+    if db_connected is True:
 
-    sensors_db = get_value_sensores( contentorId)
+        # Open db connection
+        sensors_db = get_value_sensores(contentorId)
 
+        for i in range(len(sensors_db)):
+            sensor_aux = Sensor(-1, contentorId, 'ini', 0, 0)
 
-    for i in range(len(sensors_db)):
-        sensor_aux = Sensor(-1, contentorId, 'ini', 0, 0)
+            sensor_aux.id = sensors_db[i][0]
+            sensor_aux.name = sensors_db[i][1]
+            sensor_aux.value = sensors_db[i][2]
+            sensor_aux.max = sensors_db[i][4]
+            sensor_aux.min = sensors_db[i][5]
 
-        sensor_aux.id = sensors_db[i][0]
-        sensor_aux.name = sensors_db[i][1]
-        sensor_aux.value = sensors_db[i][2]
-        sensor_aux.max = sensors_db[i][4]
-        sensor_aux.min = sensors_db[i][5]
+            sensors.append(sensor_aux)
+            data_df.append([sensor_aux.name, sensor_aux.id, sensor_aux.value, sensor_aux.min, sensor_aux.max])
 
-        sensors.append(sensor_aux)
+        df_sensors = pd.DataFrame(data_df, columns=['Type','id','Current_Value','Min','Max'])
+        df_sensors.to_csv("sensors.csv")
 
+        return sensors
 
-    return sensors
+    else:
+        df_sensors = pd.read_csv('sensors.csv')
+
+        for i in range(df_sensors.shape[0]):
+            sensor_aux = Sensor(-1, contentorId, 'ini', 0, 0)
+
+            sensor_aux.name = df_sensors.iloc[i]['Type']
+            sensor_aux.value = df_sensors.iloc[i]['Current_Value']
+            sensor_aux.min = df_sensors.iloc[i]['Min']
+            sensor_aux.max = df_sensors.iloc[i]['Max']
+            sensor_aux.id = df_sensors.iloc[i]['id']
+
+            sensors.append(sensor_aux)
+
+        return sensors
 
     # Add the sensor values to the db and also to the buffer (local memory)
 def set_sensors(sensors, time_day,temp_sens_passado):
   
   if(timing_sens < time.time() - temp_sens_passado): 
 
-     for sensor in sensors:
-        set_value_sensor(sensor.id, time_day, sensor.value)
-     temp_sens_passado = time.time()
+    if db_connected is True:
 
-    # Do later the buffer part
-     print("--- Mandou Para a DB ---") 
+        for sensor in sensors:
+            set_value_sensor(sensor.id, time_day, sensor.value)
+        temp_sens_passado = time.time()
+        
+
+    # buffer part
+    else:
+        df_data = pd.read_csv('sensor_data.csv')
+
+        for sensor in sensors:
+            row = [sensor.id, time_day, sensor.value]
+            df_data.loc[len(df_data)] = row
+
+        df_data.to_csv('sensor_data.csv', index=False)
+        temp_sens_passado = time.time()
+        
+
+    print("--- Mandou Para a DB ---") 
+
   return temp_sens_passado
 
 
@@ -69,35 +149,54 @@ def atualiza_sensores(contentorId, sensors, time, temp_sens_passado):
 
     sensors = get_sensors(sensors)
 
+    #try:
     temp_sens_passado = set_sensors(sensors, time,temp_sens_passado)
-    
+    #except:
+    #    print('Could not connect with the db')
 
     return temp_sens_passado, sensors
 
 def define_actuators(contentorId):
-
     actuators = []
+    data_df = []
 
-    # Open db connection
+    if db_connected is True:
 
-    actuators_db = get_value_atuadores( contentorId)
+        # Open db connection
+        actuators_db = get_value_atuadores(contentorId)
+
+        for i in range(len(actuators_db)):
+            actuator_aux = Actuator(-1, contentorId, 'ini', 0, 0)
+
+            actuator_aux.id = actuators_db[i][0]
+            actuator_aux.name = actuators_db[i][1]
+            actuator_aux.value = actuators_db[i][2]
+            actuator_aux.dashboard = actuators_db[i][4]
+            actuator_aux.time_passed = 0
+            actuators.append(actuator_aux)
+            data_df.append([actuator_aux.name, actuator_aux.id, actuator_aux.value, actuator_aux.dashboard])
+
+        df_actuators = pd.DataFrame(data_df, columns=['Type','id','Current_Value','dashboard'])
+        df_actuators.to_csv("actuators.csv")
+
+        return actuators
+
+    else:
+        df_actuators = pd.read_csv('actuators.csv')
+
+        for i in range(df_actuators.shape[0]):
+            actuator_aux = Actuator(-1, contentorId, 'ini', 0, 0)
+
+            actuator_aux.name = df_actuators.iloc[i]['Type']
+            actuator_aux.value = df_actuators.iloc[i]['Current_Value']
+            actuator_aux.id = df_actuators.iloc[i]['id']
+            actuator_aux.dashboard = df_actuators[i]['dashboard']
+            actuator_aux.time_passed = 0
+            actuators.append(actuator_aux)
+
+        return actuators
 
 
-    for i in range(len(actuators_db)):
-        actuator_aux = Actuator(-1, contentorId, 'ini', 0, 0)
-
-        actuator_aux.id = actuators_db[i][0]
-        actuator_aux.name = actuators_db[i][1]
-        actuator_aux.value = actuators_db[i][2]
-        actuator_aux.dashboard = actuators_db[i][4]
-        actuator_aux.time_passed = 0
-        actuators.append(actuator_aux)
-
-    return actuators
-
-def get_atuators():  #da db
-
-    return
 
 
 
@@ -156,22 +255,46 @@ def get_humidade_info(sensores):
         
     return None, None, None
 
+def add_actuator_data_buffer(value, actuator, time):
+
+    df_data = pd.read_csv('actuator_data.csv')
+
+    row = [actuator.id, time, value]
+    df_data.loc[len(df_data)] = row
+
+    df_data.to_csv('actuator_data.csv', index=False)
+    return
+
 def muda_frigorifico(valor, atuador, time):
-   # get_specific_value_atuador
-    #manda comando pro arduino
-    set_value_atuador( atuador.id, time, valor)
+    # get_specific_value_atuador
+    # manda comando pro arduino
+    if db_connected is True:
+        set_value_atuador(atuador.id, time, valor)
+
+    else:
+        add_actuator_data_buffer(valor, atuador, time)
+
     print(atuador.name, "mudou para ", valor)
     return
 
-def muda_ventoinha(valor, atuador,time):
-    #manda comando pro arduino
-    set_value_atuador( atuador.id, time, valor)
+
+def muda_ventoinha(valor, atuador, time):
+    # manda comando pro arduino
+    if db_connected is True:
+        set_value_atuador(atuador.id, time, valor)
+    else:
+        add_actuator_data_buffer(valor, atuador, time)
+
     print(atuador.name, "mudou para ", valor)
     return
 
-def muda_porta(valor, atuador,time):
-    #manda comando pro arduino
-    set_value_atuador( atuador.id, time, valor)
+
+def muda_porta(valor, atuador, time):
+    # manda comando pro arduino
+    if db_connected is True:
+        set_value_atuador(atuador.id, time, valor)
+    else:
+        add_actuator_data_buffer(valor, atuador, time)
     print(atuador.name, "mudou para ", valor)
     return
 
@@ -256,7 +379,12 @@ contentor_ids = None
 raspberry_id = 1
 time_begin_sens = [0,0]
 
-
+global db_connected
+aux_db = get_id_contentores(raspberry_id)
+if aux_db is None:
+    db_connected = False
+else:
+    db_connected = True
  
 while contentor_ids == None:
     contentor_ids = verifica_contentor(raspberry_id)
@@ -276,15 +404,21 @@ while 1:
                 # limpar terminal
     os.system('cls||clear')
 
-    time_date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    
+    aux_db = get_id_contentores(raspberry_id)
+    if aux_db is None:
+        db_connected = False
+    else:
+        db_connected = True
+        send_db_sensor_buffer()
+        send_db_actuator_buffer()
 
+    time_date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
     for i in range(len(contentor_ids)):
        print("\n-------   contentor: ", contentor_ids[i],"   -------") 
        timing_sens, timing_actu = get_timings(contentor_ids[i])
        time_begin_sens[i], sensor[i] = atualiza_sensores(contentor_ids[i], sensor[i], time_date, time_begin_sens[i])
        control_atuatores(sensor[i], atuadores[i],contentor_ids[i],time_date) 
-    time.sleep(1)
+    time.sleep(2)
        
     
