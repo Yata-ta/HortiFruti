0c393a570a73211ed5060825e74e1e2c08c3073e
gpizzigh-bit <gabrielpizzighini@gmail.com>
2023-01-16 18:41:24 +0000

    added the relays logic, create a internet cheking function and now we log data

--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@
 import colorama as cl
 import sys
 import os
+from rich.traceback import install
+install(show_locals=False)
 
 
 
+import modules.functions
 import modules.db_control
 import modules.database 
-import modules.functions
 import modules.classes
 import modules.A9comm as A9comm
 import simulator
@@ -17,13 +19,14 @@ alarm_pin = 25 ## red
 COUNTRY = "PT"
 
 contentor_ids = None
-raspberry_id = 1
 sensor = []
 sensor_values = []
 atuadores = []
 timer = []
 
 call_number = "+351910649345"
+
+host = "1.1.1.1" # local DNS server
 
 if __name__ == '__main__':
 
@@ -34,33 +37,52 @@ if __name__ == '__main__':
     
     rtn = modules.functions.initialize_system()
 
-    #modules.functions.initial_components_test()
+    #TODO retrive rooms (rapsberry_id) id from the database
+    #using 1 for now..
+    raspberry_id = 1
 
-    sensor = modules.db_control.define_sensors(1)
-    _,temp_max,temp_min = modules.db_control.get_temperatura_info(sensor)
-    _,o2_max,o2_min = modules.db_control.get_o2_info(sensor)
-    _,co2_max,co2_min = modules.db_control.get_co2_info(sensor)
-    _,hum_max,hum_min = modules.db_control.get_hum_info(sensor)
-    _,pressao_max,pressao_min = modules.db_control.get_pressao_info(sensor)
-
-    A9comm.call(call_number)
-    
+    if check_params == 2:
+        modules.functions.initial_components_test()
+        A9comm.call(call_number)
 
     if rtn == 1:
-        #TODO code functions for local execution and execute them here.
+        #Code functions for local execution and execute them here.
 
-        print("Starting local execution...")
+        print(f"Starting local execution...on room {raspberry_id}")
         modules.functions.initialize_real_sensors()
-        relay_module = modules.classes.Relay(1) # relay module of room 1
+        relay_module = modules.classes.Relay(raspberry_id) # relay module of room 1
+        print("Getting database information...")
+        try:
+            sensor = modules.db_control.define_sensors(raspberry_id)
+            print("[" + cl.Fore.GREEN + "OK" + cl.Fore.WHITE + "]" + "- Connected to the database")
+            old_signal = "ONLINE"
+        except:
+            print("[" + cl.Fore.RED + "ERROR" + cl.Fore.WHITE + "]" + "- Unable to connected to the database")
+            pass
+        try:
+            _,temp_max,temp_min = modules.db_control.get_temperatura_info(sensor)
+            _,o2_max,o2_min = modules.db_control.get_o2_info(sensor)
+            _,co2_max,co2_min = modules.db_control.get_co2_info(sensor)
+            _,hum_max,hum_min = modules.db_control.get_hum_info(sensor)
+            _,pressao_max,pressao_min = modules.db_control.get_pressao_info(sensor)
+            print("[" + cl.Fore.GREEN + "OK" + cl.Fore.WHITE + "]" + "- Obtained sensor limits")
+            limits = [temp_max,temp_min,hum_max,hum_min,o2_max,o2_min,co2_max,co2_min,pressao_max,pressao_min]
+        except:
+            print("[" + cl.Fore.RED + "ERROR" + cl.Fore.WHITE + "]" + "- Unable to obtain sensor limits...raising alarm")
+            modules.functions.alarm(alarm_pin,"on")
 
+        print("Everything is" + cl.Fore.GREEN + " OK " + cl.Fore.WHITE +"starting main loop")
         while True:
 
+            # Test internet connection
+            is_there_internet,new_signal = modules.functions.check_internet(host,old_signal)
+            old_signal = new_signal
+
             time_date = modules.database.strftime("%Y-%m-%d %H:%M:%S", modules.database.gmtime())
-            # read temperature values
+            # read sensor values
             sensor_values = modules.functions.read_real_sensors(COUNTRY)
 
-            #print(f"CO2: {sensor_values[4]}")
-            #print(f"Humidity: {sensor_values[2]}")
+
             print("-------")
             print()
             print(f"TEMP: {temp_min} < {sensor_values[0]:.2f} < {temp_max}")
@@ -68,26 +90,19 @@ if __name__ == '__main__':
             print(f"CO2: {co2_min} < {sensor_values[4]:.2f} < {co2_max}")
             print(f"HUM: {hum_min} < {sensor_values[2]:.2f} < {hum_max}")
             print(f"PRESSAO: {pressao_min} < {sensor_values[3]:.2f} < {pressao_max}")
+            print(f"internet status: {is_there_internet}")
             print()
             print("-------")
 
-            data = [f"{sensor_values[0]:.2f}",f"{modules.functions.get_OxygenValues():.2f}"]
-            modules.functions.log_data(data)
+            #Atuator logic function for chamber x
+            modules.functions.atuator_logic(raspberry_id,relay_module,sensor_values,limits)
 
-            #TODO create atuator logic function in functions...
-
-            if (sensor_values[0] >= temp_max):
-                ## Make the room colder --> turn on frizzer
-                relay_module.turn_on_relay_2()
+            if (is_there_internet): # there is no internet log the data and signal OFFLINE
+                #[time;temp;hum;o2;co2;press;k1;k3;k5;k7]
+                sensor_data = [f"{sensor_values[0]:.2f}",f"{sensor_values[2]:.2f}",f"{modules.functions.get_OxygenValues():.2f}",f"{sensor_values[4]:.2f}",f"{sensor_values[3]:.2f}"]
+                actuator_data = relay_module.get_states()
+                modules.functions.log_data(sensor_data,actuator_data)
                 pass
-                
-            if (sensor_values[0] <= temp_min):
-                ## temperature too low --> turn off frizzer
-                relay_module.turn_off_relay_2()
-                pass
-            
-            if f"{time_date[17]}{time_date[18]}" == "59": ## test alarm led 
-                modules.functions.alarm(alarm_pin,"on")
 
              # SAFETY DEBUG MODE, TO NOT FLOOD THE DATABASE
             if check_params == 1:
